04cbb95f3cebe5a93b23d13b695bd5b8f356051b
DeadSwim <66752697+DeadSwimek@users.noreply.github.com>
2025-06-10 18:32:18 +0200

    Update func.cs

--- func.cs
+++ func.cs
@@ -1,4 +1,4 @@
-﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Commands;
@@ -8,6 +8,9 @@ using CounterStrikeSharp.API.Modules.Utils;
 using System.Drawing;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using CS2MenuManager.API.Enum;
+using CS2MenuManager.API.Menu;
+using CounterStrikeSharp.API.Modules.Admin;
 
 
 namespace CustomPlugin
@@ -28,6 +31,53 @@ namespace CustomPlugin
         public void CreateNadeTrail(Vector start, CBaseGrenade grenade)
         {
             // Only in VIP Prémium
+        }
+        public void guns(CCSPlayerController? player)
+        {
+            var client = player!.Index;
+
+            if (player == null) return;
+
+            if (Guns[player.Index] == 0) { return; }
+
+            ScreenMenu menu = new($"-> {Localizer["Guns"]} <-", this);
+            menu.AddItem("M4A1", (p, option) =>
+            {
+                player.GiveNamedItem("weapon_m4a1");
+                Selected[client] = 1;
+                Selected_round[client] = 1;
+                player.PrintToChat($" {Config.Prefix} {Localizer["VIPYouChoose", "M4A1"]} ");
+            });
+            menu.AddItem("M4A1-S", (p, option) =>
+            {
+                player.GiveNamedItem("weapon_m4a1_silence");
+                Selected[client] = 2;
+                Selected_round[client] = 1;
+                player.PrintToChat($" {Config.Prefix} {Localizer["VIPYouChoose", "M4A1-S"]}");
+            });
+            menu.AddItem("AK-47", (p, option) =>
+            {
+                player.GiveNamedItem("weapon_ak47");
+                Selected[client] = 3;
+                Selected_round[client] = 1;
+                player.PrintToChat($" {Config.Prefix} {Localizer["VIPYouChoose", "AK-47"]}");
+            });
+            menu.AddItem("AWP [ M-VIP ]", (p, option) =>
+            {
+                if (AdminManager.PlayerHasPermissions(player, "@vip/mvip"))
+                {
+                    player.GiveNamedItem("weapon_awp");
+                    Selected[client] = 4;
+                    Selected_round[client] = 1;
+                    player.PrintToChat($" {Config.Prefix} {Localizer["VIPYouChoose", "AWP"]}");
+                }
+                else
+                {
+                    player.PrintToChat($" {Config.Prefix} {Localizer["AllowedFor", "MVIP"]}");
+                }
+            });
+
+            menu.Display(player, 0);
         }
     }
 }
