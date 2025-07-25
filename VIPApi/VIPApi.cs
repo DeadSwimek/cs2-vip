2b135bea94ff4f9af8486438f283d36abe1fabaa
DeadSwim <66752697+DeadSwimek@users.noreply.github.com>
2025-07-25 16:13:18 +0200

    Update VIPApi.cs

--- VIPApi/VIPApi.cs
+++ VIPApi/VIPApi.cs
@@ -1,4 +1,4 @@
-﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core;
 using System.Collections.Generic;
 using System;
 
@@ -8,4 +8,9 @@ public interface IAPI
 {
     bool IsVIP(CCSPlayerController player);
     string GetVIPGroup(CCSPlayerController player);
+    public void OpenTagMenu(CCSPlayerController player);
+    public void OpenVIPMenu(CCSPlayerController player);
+    public void OpenModelsMenu(CCSPlayerController player);
+    public void SwitchQuake(CCSPlayerController player);
+    public string GenerateVIP(CCSPlayerController target, string? type, string? Time);
 }
